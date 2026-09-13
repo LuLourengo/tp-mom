4b3ad6d6d24961bf3cb74ec24b0e47fe760a0a6f
LuLourengo <llourengo@fi.uba.ar>
2026-09-13 18:20:16 -0300

    termino con Queue

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -3,44 +3,76 @@ import random
 import string
 from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 
+
+class MessageAcknowledger:
+    def __init__(self, channel, delivery_tag):
+        self.channel = channel
+        self.delivery_tag = delivery_tag
+
+    def ack(self):
+        self.channel.basic_ack(delivery_tag=self.delivery_tag)
+
+    def nack(self):
+        self.channel.basic_nack(delivery_tag=self.delivery_tag, requeue=True)
+
+
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
-
         self.host = host
         self.queue_name = queue_name
         self.connection = None
         self.channel = None
         self.consumer_tag = None
         self.is_consuming = False
+        self._user_callback = None
 
-        try:
-            self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.host))
-            self.channel = self.connection.channel()
-            self.channel.queue_declare(queue=self.queue_name)
-        except pika.exceptions.AMQPConnectionError as err:
-            raise MessageMiddlewareDisconnectedError(f"Fallo de conexion con el broker en {self.host}: {err}")
-        except Exception as err:
-            raise MessageMiddlewareMessageError(f"No se pudo declarar la cola '{self.queue_name}': {err}")
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.host))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.queue_name)
 
     def send(self, message):
-        try:
-            self.channel.basic_publish(
-                exchange="",
-                routing_key=self.queue_name,
-                body=message,
-            )
-        except pika.exceptions.AMQPConnectionError as err:
-            raise MessageMiddlewareDisconnectedError(
-                f"Fallo de conexion al enviar a la cola '{self.queue_name}': {err}"
-            )
-        except Exception as err:
-            raise MessageMiddlewareMessageError(
-                f"Error al enviar mensaje a la cola '{self.queue_name}': {err}"
-            )
+        self.channel.basic_publish(
+            exchange="",
+            routing_key=self.queue_name,
+            body=message,
+        )
+
+    def _on_message_received(self, channel, method, properties, body):
+        handler = MessageAcknowledger(channel, method.delivery_tag)
+        self._user_callback(body, handler.ack, handler.nack)
+
+    def start_consuming(self, on_message_callback):
+        self._user_callback = on_message_callback
+        self.is_consuming = True
+        self.consumer_tag = self.channel.basic_consume(
+            queue=self.queue_name,
+            on_message_callback=self._on_message_received,
+            auto_ack=False,
+        )
+        self.channel.start_consuming()
+
+    def stop_consuming(self):
+        if not self.is_consuming:
+            return
+
+        if self.consumer_tag and self.channel.is_open:
+            self.channel.basic_cancel(self.consumer_tag)
+        if self.channel.is_open:
+            self.channel.stop_consuming()
+        self.is_consuming = False
+
+    def close(self):
+        if self.is_consuming:
+            self.stop_consuming()
+
+        if self.channel and self.channel.is_open:
+            self.channel.close()
+
+        if self.connection and self.connection.is_open:
+            self.connection.close()
+
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
+
     
-    def __init__(self, host, exchange_name, routing_keys):
-        pass
- 
